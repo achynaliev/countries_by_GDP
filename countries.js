d6c87ea85f0ebfc6acee439aed95e2f40c73b5fa
Atai Chynaliev <chynaliev.ae@gmail.com>
2018-04-19 16:32:09 -0700

    inplemented on click to show detailed GDP progression

--- countries.js
+++ countries.js
@@ -12,8 +12,37 @@ function byYear(){
 
 byYear();
 
-
 function FuckData(gdpYear) {
+
+  const countryIndex = {'USA': 'http://api.worldbank.org/v2/countries/us/indicators/NY.GDP.MKTP.CD?format=json',
+          'China': 'http://api.worldbank.org/v2/countries/cn/indicators/NY.GDP.MKTP.CD?format=json',
+        'Japan': 'http://api.worldbank.org/v2/countries/jp/indicators/NY.GDP.MKTP.CD?format=json',
+        'Germany': 'http://api.worldbank.org/v2/countries/de/indicators/NY.GDP.MKTP.CD?format=json',
+      'France': 'http://api.worldbank.org/v2/countries/fr/indicators/NY.GDP.MKTP.CD?format=json',
+      'UK': 'http://api.worldbank.org/v2/countries/gb/indicators/NY.GDP.MKTP.CD?format=json',
+      'India': 'http://api.worldbank.org/v2/countries/in/indicators/NY.GDP.MKTP.CD?format=json',
+      'Brazil': 'http://api.worldbank.org/v2/countries/br/indicators/NY.GDP.MKTP.CD?format=json',
+      'Italy': 'http://api.worldbank.org/v2/countries/it/indicators/NY.GDP.MKTP.CD?format=json',
+      "Canada": 'http://api.worldbank.org/v2/countries/ca/indicators/NY.GDP.MKTP.CD?format=json',
+      "South Korea": 'http://api.worldbank.org/v2/countries/kr/indicators/NY.GDP.MKTP.CD?format=json',
+      'Russia': 'http://api.worldbank.org/v2/countries/ru/indicators/NY.GDP.MKTP.CD?format=json',
+      'Australia': 'http://api.worldbank.org/v2/countries/au/indicators/NY.GDP.MKTP.CD?format=json',
+      'Spain': 'http://api.worldbank.org/v2/countries/es/indicators/NY.GDP.MKTP.CD?format=json',
+      'Mexico': 'http://api.worldbank.org/v2/countries/mx/indicators/NY.GDP.MKTP.CD?format=json',
+      'Indonesia': 'http://api.worldbank.org/v2/countries/id/indicators/NY.GDP.MKTP.CD?format=json',
+      'Turkey': 'http://api.worldbank.org/v2/countries/tr/indicators/NY.GDP.MKTP.CD?format=json',
+      'Netherlands': 'http://api.worldbank.org/v2/countries/nl/indicators/NY.GDP.MKTP.CD?format=json',
+      'Switzerland': 'http://api.worldbank.org/v2/countries/ch/indicators/NY.GDP.MKTP.CD?format=json',
+      'Saudi Arabia': 'http://api.worldbank.org/v2/countries/sa/indicators/NY.GDP.MKTP.CD?format=json',
+      'Argentina': 'http://api.worldbank.org/v2/countries/ar/indicators/NY.GDP.MKTP.CD?format=json',
+      'Sweden': 'http://api.worldbank.org/v2/countries/se/indicators/NY.GDP.MKTP.CD?format=json',
+      'Poland': 'http://api.worldbank.org/v2/countries/pl/indicators/NY.GDP.MKTP.CD?format=json',
+      'Belgium': 'http://api.worldbank.org/v2/countries/be/indicators/NY.GDP.MKTP.CD?format=json',
+      'Thailand': 'http://api.worldbank.org/v2/countries/th/indicators/NY.GDP.MKTP.CD?format=json',
+      'Iran': 'http://api.worldbank.org/v2/countries/ir/indicators/NY.GDP.MKTP.CD?format=json',
+      'Austria' : 'http://api.worldbank.org/v2/countries/at/indicators/NY.GDP.MKTP.CD?format=json',
+      'Egypt' : 'http://api.worldbank.org/v2/countries/eg/indicators/NY.GDP.MKTP.CD?format=json',
+      'Nigeria': 'http://api.worldbank.org/v2/countries/ng/indicators/NY.GDP.MKTP.CD?format=json'}
 
   let more_information = function (d, selectedNode) {
 
@@ -63,8 +92,17 @@ function FuckData(gdpYear) {
   }
 
   let bubbleChart = d3.select(".bubbleChart")
-  let width = 1250;
-  let height = 1220;
+  let width = 1310;
+  let height = 1150;
+
+  let ifbtn = document.getElementById("backBtn");
+  let ifdetailstext = document.getElementById("detailstext")
+  if (ifbtn) {
+    ifbtn.remove();
+  }
+  if (ifdetailstext) {
+    ifdetailstext.remove();
+  }
 
   let svg = bubbleChart
     .html("")
@@ -91,7 +129,7 @@ function FuckData(gdpYear) {
       .attr("xlink:href", "us.png")
 
 
-    var radiusScale = d3.scaleSqrt().domain([1,20000000]).range([10,650])
+    var radiusScale = d3.scaleSqrt().domain([1,20000000]).range([10,665])
 
     var simulation = d3.forceSimulation()
         .force("x", d3.forceX(width/2).strength(0.05))
@@ -104,7 +142,7 @@ function FuckData(gdpYear) {
     .defer(d3.csv, `./data/countries${gdpYear}.csv`)
     .await(ready)
 
-    let color = d3.scaleOrdinal(d3.schemeCategory10);
+    let color = d3.scaleOrdinal(d3.schemeCategory20);
 
     function ready (error, datapoints ) {
 
@@ -119,8 +157,6 @@ function FuckData(gdpYear) {
         .attr("cx", 500)
         .attr("cy", 500)
         .style("opacity", 0.95)
-        .style("stroke", "white")
-        .style('stroke-width', 1)
         .on('mouseenter', function(d){"",
               d3.selectAll("circle").style('opacity', 0.3);
               var mouseNode = d3.select(this);
@@ -145,6 +181,74 @@ function FuckData(gdpYear) {
               d3.selectAll("text").attr("visibility","visible");
               d3.selectAll("#normalText").attr("visibility","none");
               d3.selectAll(".innerText").remove();
+          })
+          .on('click', function(d) {
+            let apiurl = countryIndex[d.id]
+
+            var y = document.getElementsByClassName('barchartInfo');
+            var btn = document.createElement("BUTTON");       // Create a <button> element
+            var t = document.createTextNode("Go back");
+            var detailstext = document.createElement("H3");
+            var ttt = document.createTextNode(`GDP progression of ${d.id} for last 48 years`);
+            detailstext.appendChild(ttt);
+            detailstext.id = 'detailstext';
+
+            btn.appendChild(t);
+            document.getElementById('barchartInfo').appendChild(btn);
+            document.getElementById('barchartInfo').appendChild(detailstext)
+            btn.id = "backBtn"
+            btn.addEventListener("click", () => FuckData(gdpYear))
+
+            $.ajax({
+              type: 'GET',
+              url: `${apiurl}`,
+
+              success: function(data) {
+                let country = data[1].slice(1)
+                country = country.reverse()
+                var svg = d3.select("svg").html("").attr("width", "1400")
+                .attr("height", "600"),
+                    margin = {top: 20, right: 20, bottom: 50, left: 100},
+                    width = +svg.attr("width") - margin.left - margin.right,
+                    height = +svg.attr("height") - margin.top - margin.bottom;
+
+                var x = d3.scaleBand().rangeRound([0, width]).padding(0.2),
+                    y = d3.scaleLinear().rangeRound([height, 0]);
+
+                var g = svg.append("g")
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+                  x.domain(country.map(function(d) { return d.date; }));
+                  y.domain([0, d3.max(country, function(d) { return d.value; })]);
+
+                  g.append("g")
+                      .attr("class", "axis axis--x")
+                      .attr("transform", "translate(0," + height + ")")
+                      .call(d3.axisBottom(x));
+
+                  g.append("g")
+                      .attr("class", "axis axis--y")
+                      .call(d3.axisLeft(y).ticks(10))
+                    .append("text")
+                      .attr("transform", "rotate(-90)")
+                      .attr("y", 10)
+                      .attr("dy", "1em")
+                      .attr("text-anchor", "end")
+                      .text("Frequency");
+
+                  console.log(x.bandwidth());
+
+                  g.selectAll(".bar")
+                    .data(country)
+                    .enter().append("rect")
+                      .attr("class", "bar")
+                      .attr("x", function(d) { return x(d.date); })
+                      .attr("y", function(d) { return y(d.value); })
+                      .attr("width", x.bandwidth())
+                      .attr("height", function(d) { return height - y(d.value); });
+
+              }
+            })
           });
 
       let texts = svg.selectAll(null)
---
+++ try.js
@@ -0,0 +1,119 @@
+function byYear(){
+  let e = document.getElementById("yearSelect");
+  let gdpYear = e.options[e.selectedIndex].value;
+
+  e.addEventListener("change", function() {
+    FuckData(e.options[e.selectedIndex].value);
+  });
+
+  FuckData(e.options[e.selectedIndex].value)
+
+};
+
+byYear();
+
+function FuckData(gdpYear) {
+
+  let more_information = function (d, selectedNode) {
+
+    selectedNode.append("text")
+        .attr("class", "innerText")
+        .attr("text-anchor", "middle")
+        .attr("font-size", 30)
+        .attr("x", d.x)
+        .attr("y", d.y)
+        .attr("dy", "-3em")
+        .attr("font-weight", "500")
+        .attr("pointer-events", "none")
+        .text(d.id);
+
+    selectedNode.append("text")
+        .attr("class", "innerText")
+        .attr("text-anchor", "middle")
+        .attr("font-size", 24)
+        .attr("x", d.x)
+        .attr("y", d.y)
+        .attr("dy", "-1.2em")
+        .attr("font-weight", "500")
+        .attr("pointer-events", "none")
+        .text('GDP(nominal) $' + d.gdp2017 + " milllions");
+
+    selectedNode.append("text")
+        .attr("class", "innerText")
+        .attr("text-anchor", "middle")
+        .attr("font-size", 24)
+        .attr("x", d.x)
+        .attr("y", d.y)
+        .attr("dy", "0em")
+        .attr("font-weight", "400")
+        .attr("pointer-events", "none")
+        .text("quality of life index " + d.qualityOfLife);
+
+    selectedNode.append("text")
+        .attr("class", "innerText")
+        .attr("text-anchor", "middle")
+        .attr("font-size", 24)
+        .attr("x", d.x)
+        .attr("y", d.y)
+        .attr("dy", "1.2em")
+        .attr("font-weight", "400")
+        .attr("pointer-events", "none")
+        .text("pollution index " + d.polution);
+  }
+
+    var svg = d3.select(".bubbleChart")
+    let width = 1300;
+    let height = 1220;
+
+    var format = d3.format(",d");
+    var color = d3.scaleOrdinal(d3.schemeCategory20c);
+    var pack = d3.pack()
+        .size([width, height])
+        .padding(3);
+    d3.csv("./data/countries2017.csv", function(d) {
+      if (d.gdp2017) return d;
+    }, function(error, classes) {
+      console.log(classes);
+      if (error) throw error;
+      var root = d3.hierarchy({children: classes})
+          .sum(function(d) { return d.gdp2017; })
+          .each(function(d) {
+            if (id = d.id) {
+              var id, i = id.lastIndexOf(".");
+              d.id = id;
+              d.package = id.slice(0, i);
+              d.class = id.slice(i + 1);
+              console.log();
+            }
+          });
+
+      var node = svg.selectAll(".node")
+        .data(pack(root).leaves())
+        .enter().append("g")
+          .attr("class", "node")
+          .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+      node.append("circle")
+          .attr("id", function(d) { return d.id; })
+          .attr("r", function(d) { return d.r; })
+          .style("fill", function(d) { return color(Math.random()); });
+
+      node.append("clipPath")
+          .attr("id", function(d) { return "clip-" + d.id; })
+        .append("use")
+          .attr("xlink:href", function(d) { return "#" + d.id; });
+
+      node.append("text")
+          .attr("clip-path", function(d) { return "url(#clip-" + d.id + ")"; })
+        .selectAll("tspan")
+        .data(function(d) { return d.gdp2017.split(/(?=[A-Z][^A-Z])/g); })
+        .enter().append("tspan")
+          .attr("x", 0)
+          .attr("y", function(d, i, nodes) { return 13 + (i - nodes.length / 2 - 0.5) * 10; })
+          .text(function(d) { return d; });
+
+      node.append("title")
+          .text(function(d) { return d.id + "\n" + format(d.gdp2017); });
+    });
+
+};
